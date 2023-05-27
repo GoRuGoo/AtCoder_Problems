425f2e4515082b0edd6a31437e41b8ebea4402a2
GoRuGoo <dachanyr8989@gmail.com>
2023-05-27 21:24:53 +0900

    feat: 303

---
+++ ABC303/A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <type_traits>
+using namespace std;
+
+int main(void) {
+  int n;
+  string s;
+  string t;
+  cin >> n >> s >> t;
+  for (int i = 0; i < n; i++) {
+    if ((s[i] == '1' && t[i] == 'l') || (s[i] == 'l' && t[i] == '1')) {
+      continue;
+    } else if ((s[i] == '0' && t[i] == 'o') || (s[i] == 'o' && t[i] == '0')) {
+      continue;
+    } else if (s[i] == t[i]) {
+      continue;
+    } else {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  cout << "Yes" << endl;
+  return 0;
+}
